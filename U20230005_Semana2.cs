09909cd5590c076c5789728bdb05dee29b3a6900
Msalmeron95 <martinsstudent@gmail.com>
2023-07-30 08:45:35 -0400

    Variables

--- U20230005_Semana2.cs
+++ U20230005_Semana2.cs
@@ -26,6 +26,41 @@ namespace MyApp // Note: actual namespace depends on the project name.
             bool isStudent = true;
             bool isRecipientOfScholarship = false;
 
+            using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Declare 4 variables de tipo string que representen su nombre completo, asigne valores desde la declaración.
+            string myName = "Martin";
+            string secondName = "de Jesus";
+            string lastName = "Salmeron";
+            string maidenName = "Pastora";
+
+            // 2) Declare 3 variables de tipo integer que tengan relación con la edad ya sea la suya y de sus padres, asigne valores desde la declaración.
+            int myAge = 28;
+            int fatherAge = 60;
+            int motherAge = 59;
+
+            // 3) Declare 3 variables de tipo float que representen sus gastos más frecuentes como por ejemplo queda a libre elección los gastos a representar, asigne valores desde la declaración.
+            float monthlyRent = 1200.50f;
+            float groceryExpenses = 350.75f;
+            float transportationExpenses = 150.25f;
+
+            // 4) Declare 2 variables de tipo boolean para representar su estado como estudiante en esta materia y otra si es becado, asigne valores desde la declaración.
+            bool isStudent = true;
+            bool isRecipientOfScholarship = false;
+
+            Console.WriteLine("Hola {0} {1} {2} {3}", myName, secondName, lastName, maidenName);
+
+            //Autoevaluacion 8
+        }
+    }
+}
+
             //Autoevaluacion 8
         }
     }
